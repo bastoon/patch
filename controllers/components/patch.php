7301cd78e3a7afefb2674c4cb274fad956b75b19
Bastien Wethli on dev.weth.li <dev@bastoon.ch>
2011-06-11 12:50:47 +0200

    nothing important

--- controllers/components/patch.php
+++ controllers/components/patch.php
@@ -2,64 +2,65 @@
 class PatchComponent extends Object {
 	var $components = array('Auth','Email');
 
-        public function getUsername(){
-                $aUser = $this->Auth->user();
-                return $aUser['User']['name'];
-        }
+	public function getUsername(){
+		$aUser = $this->Auth->user();
+		return $aUser['User']['name'];
+	}
 
-        public function getAdmingroup(){
-                $aUser = $this->Auth->user();
-                return $aUser['User']['admingroupe_id'];
-        }
+	public function getAdmingroup(){
+		$aUser = $this->Auth->user();
+		return $aUser['User']['admingroupe_id'];
+	}
 
-        public function getId(){
-                $aUser = $this->Auth->user();
-                return $aUser['User']['id'];
-        }
+	public function getId(){
+		$aUser = $this->Auth->user();
+		return $aUser['User']['id'];
 
-        public function IsAdmin(){
-                if ( $this->getAdmingroup() == '1' ){
-                        $IsAdmin = True;
-                } else {
-                        $IsAdmin = False;
-                }
-                return $IsAdmin;
-        }
+	}
 
-        public function IsOwner($Current_Controller){
+	public function IsAdmin(){
+		if ( $this->getAdmingroup() == '1' ){
+			$IsAdmin = True;
+		} else {
+			$IsAdmin = False;
+		}
+		return $IsAdmin;
+	}
+
+	public function IsOwner($Current_Controller){
 		$IsOwner = False;
 		$Controller_name = ucfirst($Current_Controller->name);
 		$BelongsTo = $Current_Controller->belongsTo['User']['foreignKey'];
-                $Owner_id = $Current_Controller->data[ $Controller_name ][ $BelongsTo ];
-                if ( $Owner_id == $this->getId() ) { $IsOwner = True; }
-                if ( $this->IsAdmin() ) { $IsOwner = True; }
-                return $IsOwner;
-        }
+		$Owner_id = $Current_Controller->data[ $Controller_name ][ $BelongsTo ];
+		if ( $Owner_id == $this->getId() ) { $IsOwner = True; }
+		if ( $this->IsAdmin() ) { $IsOwner = True; }
+		return $IsOwner;
+	}
 
-        public function sendInviteMail($to, $token) {
+	public function sendInviteMail($to, $token) {
 		$body[0] = 'Welcome ';
 		$body[1] = 'http://patch.weth.li/users/validmail?token='.$token;
 		$subject = 'Inscription';
-                if ( $this->sendMail($to, $body, $subject) ) {
+		if ( $this->sendMail($to, $body, $subject) ) {
 			$body[0] = "Nouvel inscrit : ".$to;
 			$this->sendMail('bastoon@bastoon.ch', $body, $subject);
 			return True;
-                } else {
+		} else {
 			return False;
-                }
-        }
-	
+		}
+	}
+
 	private function sendMail($to, $body, $subject){
-                $this->Email->reset();
-                $this->Email->to = $to;
-                $this->Email->subject = $subject;
-                $this->Email->replyTo = 'patch@weth.li';
-                $this->Email->from = 'Fiche Technique <'.$this->Email->replyTo.'>';
-                if ( $this->Email->send($body) ) {
-                        return True;
-                } else {
-                        return False;
-                }
+		$this->Email->reset();
+		$this->Email->to = $to;
+		$this->Email->subject = $subject;
+		$this->Email->replyTo = 'patch@weth.li';
+		$this->Email->from = 'Fiche Technique <'.$this->Email->replyTo.'>';
+		if ( $this->Email->send($body) ) {
+			return True;
+		} else {
+			return False;
+		}
 	}
 
 
@@ -68,13 +69,13 @@ class PatchComponent extends Object {
 	}
 
 
-        private function getRandomString($length) {
-                $chars = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
-                $size = strlen( $chars );
-                $str = null;
-                for( $i = 0; $i < $length; $i++ ) {
-                        $str .= $chars[ rand( 0, $size - 1 ) ];
-                }
-                return $str;
-        }
+	private function getRandomString($length) {
+		$chars = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
+		$size = strlen( $chars );
+		$str = null;
+		for( $i = 0; $i < $length; $i++ ) {
+			$str .= $chars[ rand( 0, $size - 1 ) ];
+		}
+		return $str;
+	}
 }
--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -5,42 +5,42 @@ class UsersController extends AppController {
 	var $components = array('Patch');
 
 	function beforeFilter(){
-        	parent::beforeFilter();
-    	}
-	 
+		parent::beforeFilter();
+	}
+
 	/*
-	* Action reservÃ©e aux utilisateurs logués
-	* @param    -
-	* @return   -
-	**/
-	    
+	 * Action reservÃ©e aux utilisateurs logués
+	 * @param    -
+	 * @return   -
+	 **/
+
 	function bo_accueil(){
 		if($this->Auth->user()){
 			$this->set('Username', $this->Patch->getUsername());
 			$this->set('IsAdmin',$this->Patch->IsAdmin());
-            $this->set('Id',$this->Patch->getId());
-	 	}
+			$this->set('Id',$this->Patch->getId());
+		}
 	}
-	 
+
 	/*
-	* Action reservÃ©e aux utilisateurs loguÃ©s & du groupe membre
-	* @param    -
-	* @return   -
-	**/
+	 * Action reservÃ©e aux utilisateurs loguÃ©s & du groupe membre
+	 * @param    -
+	 * @return   -
+	 **/
 	function bo_action_admin(){
 		if($this->Auth->user()){
 			$this->set('Username', $this->Patch->getUsername());
 			$this->set('IsAdmin',$this->Patch->IsAdmin());
 			$this->set('Id',$this->Patch->getId());
 		}
-//		print_r($this->params);	 
+		//		print_r($this->params);
 	}
-	 
+
 	/*
-	* Login
-	* @param    -
-	* @return   -
-	**/
+	 * Login
+	 * @param    -
+	 * @return   -
+	 **/
 	function login(){
 		// VÃ©rifie que le compte est activÃ© (basÃ© sur l'userscope)
 		if($this->action == 'login' && !empty($this->data['User']['email'])){
@@ -48,34 +48,34 @@ class UsersController extends AppController {
 			if($this->User->find('count', array('conditions' => $conditions))){
 				$this->Session->setFlash(__('Votre compte n\'a pas Ã©tÃ© activÃ©',true), 'default', array(), 'auth');
 			}
-	    }
+		}
 	}
-	 
+
 	/*
-	* Logout
-	* @param    -
-	* @return   -
-	**/
+	 * Logout
+	 * @param    -
+	 * @return   -
+	 **/
 	function logout(){
 		$this->Session->delete('acl_user');
 		$this->redirect($this->Auth->logout());
 	}
 
 	function validmail($id = null){
-                if (!empty($this->params['url']['token'])) {
+		if (!empty($this->params['url']['token'])) {
 			$UserId = $this->User->find('first', array('conditions' => array('User.token' =>  $this->params['url']['token'])));
 			if ($UserId['User']['id']){
 				$this->User->read(null, $UserId['User']['id']);
 				$this->User->set('actif','1');
-	                        $this->User->set('token', null);
+				$this->User->set('token', null);
 				$this->User->save();
 				// Create contact/ SoundPatch / Tracks
-		                $this->tpl_pack_1p_12t_5c($UserId['User']['id'], 'Pack de base');
+				$this->tpl_pack_1p_12t_5c($UserId['User']['id'], 'Pack de base');
 				$this->redirect($this->Auth->login());
 			} else {
 				$this->Session->setFlash(__('TOKEN not FOUND !', true));
 			}
-                }
+		}
 	}
 
 	function index() {
@@ -99,7 +99,7 @@ class UsersController extends AppController {
 			if ($this->User->save($this->data)) {
 				$this->Session->setFlash(__('The User has been saved', true));
 				$this->Patch->sendInviteMail($this->data['User']['email'], $token);
- 				$this->Session->setFlash('Veuillez consulter votre mail pour activer votre compte en ciquant sur le lien.');
+				$this->Session->setFlash('Veuillez consulter votre mail pour activer votre compte en ciquant sur le lien.');
 				$this->redirect(array('action'=>'login'));
 			} else {
 				$this->Session->setFlash(__('The User could not be saved. Please, try again.', true));
@@ -140,48 +140,48 @@ class UsersController extends AppController {
 		}
 	}
 
-        private function tpl_pack_1p_12t_5c($id, $name) {
-                $this->tpl_contact_5($id);
-                $this->tpl_patch_1p_12t($id, $name);
-        }
+	private function tpl_pack_1p_12t_5c($id, $name) {
+		$this->tpl_contact_5($id);
+		$this->tpl_patch_1p_12t($id, $name);
+	}
 
-        private function tpl_patch_1p_12t($id, $name) {
-                $this->tpl_create_patch($id, 1, 12, $name);
-       }
+	private function tpl_patch_1p_12t($id, $name) {
+		$this->tpl_create_patch($id, 1, 12, $name);
+	}
 
-        private function tpl_create_patch($id, $nb_p, $nb_t, $name = null){
-                for( $i = 0; $i < $nb_p; $i++ ) {
-                        $this->User->Soundpatch->create();
-                        $this->User->Soundpatch->set('title',$name);
-                        $this->User->Soundpatch->set('user_id',$id);
-                        $this->User->Soundpatch->save();
-                        $new_soundpatch_id = $this->User->Soundpatch->id;
-                        $this->tpl_create_track($id, $nb_t, $new_soundpatch_id);
-                }
-        }
+	private function tpl_create_patch($id, $nb_p, $nb_t, $name = null){
+		for( $i = 0; $i < $nb_p; $i++ ) {
+			$this->User->Soundpatch->create();
+			$this->User->Soundpatch->set('title',$name);
+			$this->User->Soundpatch->set('user_id',$id);
+			$this->User->Soundpatch->save();
+			$new_soundpatch_id = $this->User->Soundpatch->id;
+			$this->tpl_create_track($id, $nb_t, $new_soundpatch_id);
+		}
+	}
 
-        private function tpl_create_track($id, $nb, $sp_id){
-                for( $i = 1; $i < $nb+1; $i++ ) {
+	private function tpl_create_track($id, $nb, $sp_id){
+		for( $i = 1; $i < $nb+1; $i++ ) {
 			$this->User->Soundpatch->Track->create();
-                        $this->User->Soundpatch->Track->set('rank',$i);
-                        $this->User->Soundpatch->Track->set('soundpatch_id',$sp_id);
-                        $this->User->Soundpatch->Track->save();
-                }
-        }
+			$this->User->Soundpatch->Track->set('rank',$i);
+			$this->User->Soundpatch->Track->set('soundpatch_id',$sp_id);
+			$this->User->Soundpatch->Track->save();
+		}
+	}
 
-        private function tpl_create_contact($id, $nb){
-                for( $i = 0; $i < $nb; $i++ ) {
-                        $this->User->Contact->create();
-                        $this->User->Contact->set('name','Nom du contact');
-                        $this->User->Contact->set('email','');
-                        $this->User->Contact->set('phone','');
-                        $this->User->Contact->set('user_id',$id);
-                        $this->User->Contact->save();
-                }
-        }
+	private function tpl_create_contact($id, $nb){
+		for( $i = 0; $i < $nb; $i++ ) {
+			$this->User->Contact->create();
+			$this->User->Contact->set('name','Nom du contact');
+			$this->User->Contact->set('email','');
+			$this->User->Contact->set('phone','');
+			$this->User->Contact->set('user_id',$id);
+			$this->User->Contact->save();
+		}
+	}
 
-        private function tpl_contact_5($id) {
-                $this->tpl_create_contact($id, 5);
-        }
+	private function tpl_contact_5($id) {
+		$this->tpl_create_contact($id, 5);
+	}
 }
 ?>
